f101e418b079a0c2a96930093db56d24aa2af960
Paolo Venturi <paolo.venturi.91@gmail.com>
2018-08-19 08:49:08 +0200

    Visualizzazione degli eventi selezionabili nella schermata di login

--- renderers/login_renderer.js
+++ renderers/login_renderer.js
@@ -1,1 +1,30 @@
 const { remote } = require('electron')
+
+const zerorpc = require("zerorpc")
+let client = new zerorpc.Client()
+
+client.connect("tcp://127.0.0.1:4243")
+
+client.invoke("echo", "server ready", (error, res) => {
+   if(error || res !== 'server ready') {
+      console.error(error)
+   } else {
+      console.log("server is ready")
+   }
+})
+
+client.invoke("getEvents", (error, res) => {
+   if(error) {
+      console.error(error)
+   } else {
+      res.forEach(event => showEvent(event));
+   }
+})
+
+function showEvent(event){
+   const container = document.getElementById('eventList');
+   const button = document.createElement('button')
+   button.setAttribute('class', 'eventButton')
+   button.innerHTML = event['name'] + ' ❯'
+   container.appendChild(button);
+}

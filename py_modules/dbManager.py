417d7f453bb5b0e2e392127a41ab56448db5f547
Paolo Venturi <paolo.venturi.91@gmail.com>
2018-08-19 08:48:09 +0200

    Aggiunto modulo python per la gestione del db

---
+++ py_modules/dbManager.py
@@ -0,0 +1,29 @@
+import os, sqlite3
+
+DB_FILENAME = 'db/EventSelling.db'
+SCHEMA_FILENAME = 'db/EventSelling.sql'
+
+class DbManager():
+   def __init__(self):
+      if not os.path.exists(DB_FILENAME):
+         with sqlite3.connect(DB_FILENAME) as conn:
+            with open(SCHEMA_FILENAME, 'rt') as f:
+               schema = f.read()
+            conn.executescript(schema)
+
+      self.db_conn = sqlite3.connect(DB_FILENAME)
+      self.db_conn.row_factory = _dict_factory
+
+   def __exit__(self):
+      self.db_conn.close()
+
+   def getEvents(self):
+      c = self.db_conn.cursor()
+      c.execute('select * from events')
+      return c.fetchall()
+
+def _dict_factory(cursor, row):
+   d = {}
+   for idx, col in enumerate(cursor.description):
+      d[col[0]] = row[idx]
+   return d
